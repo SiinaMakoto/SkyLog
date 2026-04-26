2f94f02cc1f49bf0b008efff58c01aea6a9bc988
SiinaMakoto <ym2y67yvfh@privaterelay.appleid.com>
2026-04-27 00:18:59 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -1,5 +1,37 @@
 const CACHE = "skylog-cache-v1";
+function render() {
+  const list = document.getElementById("list");
+  let data = JSON.parse(localStorage.getItem("skylog") || "[]");
 
+  if (currentTag) {
+    data = data.filter(e => e.tags.includes(currentTag));
+  }
+
+  list.innerHTML = "";
+
+  data.forEach(e => {
+    const div = document.createElement("div");
+    div.className = "card entry";
+
+    const tagHTML = e.tags.map(tag =>
+      `<span class="tag" onclick="filterTag('${tag}')">${tag}</span>`
+    ).join(" ");
+
+    div.innerHTML = `
+      <div>${e.mood || ""}</div>
+      <div>${e.text}</div>
+      <div class="small">${tagHTML}</div>
+      <div class="small">${e.date}</div>
+    `;
+
+    list.appendChild(div);
+  });
+}
+function filterTag(tag) {
+  currentTag = tag;
+  render();
+}
+let currentTag = null;
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => {
